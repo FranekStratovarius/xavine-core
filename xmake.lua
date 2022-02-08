54402c9ab72d07ce4d04dcc37e2c01b23284268f
FranekStratovarius <32364106+FranekStratovarius@users.noreply.github.com>
2022-02-08 13:51:32 +0100

    imgui hinzugefügt

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,16 @@
 add_rules("mode.debug", "mode.release")
-add_requires("glfw")
+add_requires("glfw","imgui")
 
 target("xavine")
 	set_kind("binary")
-	add_files("src/*.c")
+	add_files("src/*.cpp")
+	add_files("src/imgui/*.cpp")
 	--add_includedirs("/usr/include", "/usr/local/include")
 	add_includedirs("include")
-	set_warnings("all", "error")
+	--set_warnings("all", "error")
+	set_warnings("error")
 	--set_optimize("fastest")
 	if is_plat("linux") then
 		add_syslinks("dl")
 	end
-	add_packages("glfw")
+	add_packages("glfw","imgui")
